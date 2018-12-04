519117010a49507dc377aca96dac73ea846735e7
1391078749 <1391078749@qq.com>
2018-12-04 22:00:41 +0800

    1

---
+++ src/main/java/com/three/memory/MemoryApplication.java
@@ -0,0 +1,12 @@
+package com.three.memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryApplication.class, args);
+    }
+}
