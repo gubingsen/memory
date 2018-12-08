c4cee383684d9ee8755d15af77152cfa267e5aed
1391078749 <1391078749@qq.com>
2018-12-08 21:28:46 +0800

    1

--- src/main/java/com/three/memory/util/ResultUtil.java
+++ src/main/java/com/three/memory/util/ResultUtil.java
@@ -3,6 +3,8 @@ package com.three.memory.util;
 import com.three.memory.constant.SystemCon;
 import com.three.memory.vo.ResultBean;
 
+import java.util.List;
+
 public class ResultUtil {
 
     //操作成功
@@ -39,5 +41,37 @@ public class ResultUtil {
         }else{
             return setError(SystemCon.RERROR1,msg+"失败",null);
         }
+    }
+
+    //封装分页信息
+    public static ResultBean setPage(Object token, Object data, String msg, List list, long total,
+                                     int pages, int pageNum, int pageSize) {
+        ResultBean resultBean=new ResultBean();
+        resultBean.setCode(SystemCon.ROK);
+        resultBean.setData(data);
+        resultBean.setToken(token);
+        resultBean.setMsg(msg);
+        resultBean.setList(list);
+        resultBean.setCount(total);
+        resultBean.setPages(pages);
+        resultBean.setPageNum(pageNum);
+        resultBean.setPageSize(pageSize);
+        return resultBean;
+    }
+
+    public static int getPageNum(Integer pageNum) {
+        if (pageNum == null) {
+            pageNum = SystemCon.PAGENUM;
+        }
+
+        return pageNum;
+    }
+
+    public static int getPageSize(Integer pageSize) {
+        if (pageSize == null) {
+            pageSize = SystemCon.PAGESIZE;
+        }
+
+        return pageSize;
     }
 }
